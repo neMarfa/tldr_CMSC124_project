647efbc438fe5c821b0f8047b934c59e15488f54
neMarfa <nemarfa@up.edu.ph>
2025-11-03 19:53:19 +0800

    feat: initial commit

---
+++ src/lexer.py
@@ -0,0 +1,15 @@
+#################################
+# TOKENS
+#################################
+
+
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+    
+    def __repr__(self):
+        if self.value return f'{self.type}:{self.value}'
+        return f'{self.type}'
